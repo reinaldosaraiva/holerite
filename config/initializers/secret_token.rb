17f873ce2d5fac5770fe463e6a969868ac48187b
Reinaldo Saraiva <reinaldo.saraiva@gmail.com>
2011-01-05 11:30:08 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holerite::Application.config.secret_token = '37e08722a9853fb44067c95fd0b4568fc30b4d38907dcd8b750b459948f41c0f97adeb9396b45b6e5a280c58ffaaa7cef2f7fa26127d3684a6757833326a5f87'
